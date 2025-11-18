04ed4f38f4e197750f664a59c4b345dfd00bb37f
Majid <khan2614004@gmail.com>
2025-11-18 20:32:00 +0500

    Delete chatnext directory

--- chatnext/chatnext/doctype/chatnext_feedback/chatnext_feedback.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Umair Wali and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Chatnext Feedback", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- chatnext/chatnext/doctype/knowledge_base_article/knowledge_base_article.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Umair Wali and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class KnowledgeBaseArticle(Document):
-	pass
